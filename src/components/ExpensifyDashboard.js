ccc57ec465cdfe626ea454bafad352b26b2c5610
Taslim A. Hussain <taslim@outlook.co.nz>
2019-10-26 19:19:53 +1300

    Dev done

--- src/components/ExpensifyDashboard.js
+++ src/components/ExpensifyDashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpensifyDashboard = props => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
